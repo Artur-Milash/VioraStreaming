62d4303a45c5e2723c8d6d95a926c8e12a514574
Добрянський Володимир <volodymyr.dobrianskyi1@gmail.com>
2026-05-24 17:11:11 +0300

    feat: vs-138 add error handling

--- viora-streaming-client/src/components/Layout/AppLayoutWithSideNav.tsx
+++ viora-streaming-client/src/components/Layout/AppLayoutWithSideNav.tsx
@@ -2,17 +2,20 @@ import {Header} from "../Header/Header.tsx";
 import {Outlet} from "react-router-dom";
 import {Box, Stack} from "@mui/material";
 import {SidebarPanel} from "../Sidebar/SidebarPanel.tsx";
+import ProtectedRoute from "../../router/ProtectedRoute.tsx";
 
 export function AppLayoutWithSideNav() {
   return (
       <>
-        <Header/>
-        <Stack direction="row">
-          <SidebarPanel/>
-          <Box sx={{flex: 1}}>
-            <Outlet/>
-          </Box>
-        </Stack>
+        <ProtectedRoute>
+          <Header/>
+          <Stack direction="row">
+            <SidebarPanel/>
+            <Box sx={{flex: 1}}>
+              <Outlet/>
+            </Box>
+          </Stack>
+        </ProtectedRoute>
       </>
   );
 
--- viora-streaming-client/src/router/ProtectedRoute.tsx
+++ viora-streaming-client/src/router/ProtectedRoute.tsx
@@ -2,17 +2,20 @@ import {Navigate} from 'react-router-dom';
 import {API_PAGE} from "../constants/routingConstants.ts";
 import {AppLayout} from "../components/Layout/AppLayout.tsx";
 import {getToken} from "../utils/apiUtils.ts";
+import type {ReactNode} from "react";
 
-const ProtectedRoute = () => {
+interface ProtectedRouteProps {
+  children?: ReactNode;
+}
+
+const ProtectedRoute = ({children}: ProtectedRouteProps) => {
   const token = getToken();
 
   if (!token) {
     return <Navigate to={API_PAGE.Auth} replace/>
   }
 
-  return (
-      <AppLayout/>
-  )
+  return children ? <>{children}</> : <AppLayout/>;
 };
 
 export default ProtectedRoute;
