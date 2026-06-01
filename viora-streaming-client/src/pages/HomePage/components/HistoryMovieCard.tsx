75cda0078d916993bc6cc87e4785ca88327699ff
Добрянський Володимир <volodymyr.dobrianskyi1@gmail.com>
2026-06-01 19:26:33 +0300

    fix: fix cards overlap issue, add click handler for history card

--- viora-streaming-client/src/pages/HomePage/components/HistoryMovieCard.tsx
+++ viora-streaming-client/src/pages/HomePage/components/HistoryMovieCard.tsx
@@ -14,6 +14,7 @@ export function HistoryMovieCard({history, onClick}: HistoryMovieCardProps) {
   return (
       <Stack onClick={onClick} spacing="16px" sx={{
         cursor: "pointer",
+        width: "100%",
         minWidth: 0
       }}>
         <Box sx={{position: "relative"}}>
